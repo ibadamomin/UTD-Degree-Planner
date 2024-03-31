3e7edffb234f47701addd4caa30f702394bd0e04
ayushkasaju <118084751+ayushkasaju@users.noreply.github.com>
2024-03-30 20:05:33 -0500

    basic login screen skeleton

---
+++ pages/index.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+
+<title>Login</title>
+<link rel="stylesheet" type="text/css" href="/includes/style.css">
+<div class="index">
+    <div class="login">
+        <h2>Welcome!</h2>
+        <h3>Enter your UTD email and password</h3>
+        <form method="post">
+            <div class="input-wrapper">
+                <input type="text" id="email" name="email" required>
+                <label for="email">Email</label>
+            </div>
+            <div class="input-wrapper">
+                <input type="password" id="password" name="password" required>
+                <label for="password">Password</label>
+            </div>
+            <input type="submit" value="Login">
+        </form>
+    </div>
+</div>
