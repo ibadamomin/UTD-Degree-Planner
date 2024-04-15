45ffa0a31cb7e8646b534303b44910c1df0f8a76
nick-donovan-utd <158313767+nick-donovan-utd@users.noreply.github.com>
2024-04-15 17:06:39 -0500

    feat: adding basic html/php templates

---
+++ public/index.php
@@ -0,0 +1,20 @@
+<?php
+session_start();
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head><title>Index</title></head>
+
+<body>
+<?php
+if (isset($_SESSION['user_id'])) {
+    print "<p>Welcome " . htmlspecialchars($_SESSION['user_id']) . "</p>";
+}
+?>
+<a href="login.php">Login</a>
+<a href="logout.php">Logout</a>
+
+</body>
+
+</html>
---
+++ public/login.php
@@ -0,0 +1,67 @@
+<?php
+
+require_once "../models/User.php";
+require_once "../includes/Database.php";
+
+use DegreePlanner\Models\User\User;
+
+require_once __DIR__ . '/../vendor/autoload.php';
+
+$dotenv = Dotenv\Dotenv::createImmutable(__DIR__ . '/../');
+$dotenv->load();
+
+session_start();
+
+if (isset($_SESSION['user_id'])) {
+    header("Location: index.php");
+    exit;
+}
+
+
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    $net_id = trim($_POST['net_id']);
+    $password = $_POST['password'];
+
+    if (empty($net_id) || empty($password)) {
+        header("Location: login.php?error=Invalid username or password");
+        exit();
+    }
+    $user = User::login($net_id, $password);
+    if (!$user) {
+        header("Location: login.php?error=Invalid username or password");
+        exit();
+    }
+
+    $_SESSION['user_id'] = htmlspecialchars($user->id);
+    header("Location: index.php");
+    exit();
+
+}
+
+?>
+
+
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Login</title>
+</head>
+<body>
+<form id="login" action="login.php" method="POST">
+    <label for="net_id">Net ID: </label>
+    <input type="text" id="net_id" name="net_id" required>*<br>
+
+    <label for="password">Password: </label>
+    <input type="password" id="password" name="password" required>*<br>
+
+    <button type="submit" value="Submit">Submit</button>
+
+</form>
+<?php
+if (isset($_GET['error'])) {
+    echo "<p style='color: red;'>" . $_GET['error'] . "</php>";
+}
+?>
+</body>
+</html>
+
