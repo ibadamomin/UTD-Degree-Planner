137567ab0d3f97e6ab559760e3a2317fe384fb19
Nick Donovan <18464752-nick-donovan@users.noreply.gitlab.com>
2024-04-21 20:16:54 -0500

    feat: adding registration

--- public/register.php
+++ public/register.php
@@ -1,1 +1,85 @@
 <?php
+require_once __DIR__ . '/../vendor/autoload.php';
+
+use DegreePlanner\Models\User\User;
+
+session_start();
+
+if (isset($_SESSION['user_id'])) {
+    header("Location: index.php");
+    exit;
+}
+
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    // Sanitize input
+    $net_id = htmlspecialchars(trim($_POST['net_id']));
+    $email = filter_input(INPUT_POST, 'email', FILTER_VALIDATE_EMAIL);
+    $password = $_POST['password'];
+    $first_name = htmlspecialchars(trim($_POST['first_name']));
+    $middle_name = htmlspecialchars(trim($_POST['middle_name']));
+    $last_name = htmlspecialchars(trim($_POST['last_name']));
+    $role = ($_POST['role'] === 'faculty') ? 'faculty' : 'student';
+
+    // Make sure the required fields aren't empty.
+    if (empty($net_id) || empty($email) || empty($password) || empty($first_name) || empty($last_name)) {
+        header("Location: register.php?error=Invalid username or password");
+        exit();
+    }
+
+    $result = User::register($net_id, $email, $password, $first_name, $middle_name, $last_name, $role);
+    if ($result === true) {
+        $_SESSION['user_id'] = $net_id;
+    } else {
+        print $result;
+        header("Location: register.php?error=$result");
+        exit();
+    }
+
+    // Redirect user
+    header("Location: index.php");
+    exit();
+}
+?>
+
+
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Login</title>
+</head>
+<body>
+<form id="register" action="register.php" method="POST">
+    <label for="net_id">Net ID: </label>
+    <input type="text" id="net_id" name="net_id" required>*<br>
+
+    <label for="email">Email: </label>
+    <input type="email" id="email" name="email" required>*<br>
+
+    <label for="password">Password: </label>
+    <input type="password" id="password" name="password" required>*<br>
+
+    <label for="first_name">First Name: </label>
+    <input type="text" id="first_name" name="first_name" required>*<br>
+
+    <label for="middle_name">Middle Name: </label>
+    <input type="text" id="middle_name" name="middle_name"><br>
+
+    <label for="last_name">Last Name: </label>
+    <input type="text" id="last_name" name="last_name" required>*<br>
+
+    <label for="role">Role: </label>
+    <select name="role" id="role" required>
+        <option value="student">Student</option>
+        <option value="faculty">Faculty</option>
+    </select>*<br>
+
+    <button type="submit" value="Submit">Submit</button>
+
+</form>
+<?php
+if (isset($_GET['error'])) {
+    echo "<p style='color: red;'>" . htmlspecialchars($_GET['error']) . "</php>";
+}
+?>
+</body>
+</html>
--- src/Models/User/User.php
+++ src/Models/User/User.php
@@ -12,11 +12,13 @@ class User {
     public $middle_name;
     public $last_name;
     public $role;
+    public $advisor;
 
     public static function findUserByNetId($net_id) {
         $db = new Database();
         $db = $db->db();
-        $q = "SELECT net_id, email, password_hash FROM users WHERE net_id = ? LIMIT 1";
+
+        $q = "SELECT * FROM users WHERE net_id = ? LIMIT 1";
         $stmt = $db->prepare($q);
 
         if (!$stmt) {
@@ -33,15 +35,56 @@ class User {
             $db->close();
             return null;
         }
-        $user = $result->fetch_assoc();
+        $userDetails = $result->fetch_assoc();
         $stmt->close();
+
+        $userDetails = self::getRole($db, $userDetails);
         $db->close();
 
-        return new User($user);
+        return $userDetails != null ? new User($userDetails) : null;
+
     }
 
-    public static function register($net_id) {
-        return self::findUserByNetId($net_id);
+    private static function getRole($db, $userDetails) {
+        $net_id = $userDetails['net_id'];
+        if ($net_id == null) {
+            return null;
+        }
+
+        // Check if they're a student
+        $role = self::getUserRole($db, $net_id, 'students');
+        if ($role) {
+            $userDetails['role'] = 'student';
+            $userDetails['advisor_id'] = $role['advisor_id'];
+            return $userDetails;
+        }
+
+        // Check if they're faculty
+        $role = self::getUserRole($db, $net_id, 'faculty');
+        if ($role) {
+            $userDetails['role'] = 'faculty';
+            $userDetails['advisor_id'] = null;
+            return $userDetails;
+        }
+
+        return null;
+
+    }
+
+    private static function getUserRole($db, $net_id, $tableName) {
+        $q = "SELECT * FROM $tableName WHERE net_id = ? LIMIT 1";
+        $stmt = $db->prepare($q);
+
+        if (!$stmt) {
+            return null;
+        }
+
+        $stmt->bind_param("s", $net_id);
+        $stmt->execute();
+
+        $result = $stmt->get_result();
+        return $result->num_rows > 0 ? $result->fetch_assoc() : null;
+
     }
 
     public static function login($net_id, $password): ?User {
@@ -57,9 +100,67 @@ class User {
         return password_verify($password, $password_hash) ? $user : null;
     }
 
-    public function __construct($user) {
-        $this->id = $user['net_id'];
-        $this->email = $user['email'];
-        $this->password_hash = $user['password_hash'];
+    public static function register($net_id, $email, $password, $first_name, $middle_name, $last_name, $role) {
+        $db = new Database();
+        $db = $db->db();
+
+        $password_hash = password_hash($password, PASSWORD_ARGON2ID);
+
+        $q = "INSERT INTO users "
+            . "(net_id, email, password_hash, first_name, middle_name, last_name)"
+            . "VALUES (?, ?, ?, ?, ?, ?)";
+        $stmt = $db->prepare($q);
+
+        if (!$stmt) {
+            $db->close();
+            return "Could not complete registration!";
+        }
+
+        $stmt->bind_param("ssssss", $net_id, $email, $password_hash, $first_name, $middle_name, $last_name);
+
+        try {
+            $stmt->execute();
+        } catch (\mysqli_sql_exception $e) {
+            $error = $e->getMessage();
+            if (strpos($error, 'Duplicate entry') !== false && strpos($error, 'users.PRIMARY') !== false) {
+                $msg = "This Net ID is already registered!";
+            } elseif (strpos($error, 'Duplicate entry') !== false && strpos($error, 'users.uc_email') !== false) {
+                $msg = "This email is already in use!";
+            } else {
+                $msg = "Could not complete registration!";
+            }
+            $stmt->close();
+            $db->close();
+            return $msg;
+        }
+        $stmt->close();
+
+        if ($role === 'student') {
+            $table = 'students';
+        } else {
+            $table = 'faculty';
+        }
+
+        $q = "INSERT INTO $table (net_id) VALUES (?)";
+        $stmt = $db->prepare($q);
+        $stmt->bind_param("s", $net_id);
+        $stmt->execute();
+
+        $stmt->close();
+        $db->close();
+
+        return true;
     }
+
+    public function __construct($userDetails) {
+        $this->id = $userDetails['net_id'];
+        $this->email = $userDetails['email'];
+        $this->password_hash = $userDetails['password_hash'];
+        $this->first_name = $userDetails['first_name'];
+        $this->middle_name = $userDetails['middle_name'];
+        $this->last_name = $userDetails['last_name'];
+        $this->role = $userDetails['role'];
+        $this->advisor = $userDetails['advisor_id'];
+    }
+
 }
