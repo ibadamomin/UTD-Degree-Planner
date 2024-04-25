0b85fa466b014f1f0ee6cef76c292f4273304b88
MrHonor <danielamezquita5101@gmail.com>
2024-04-25 16:35:59 -0500

    Seafoam color

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,12 @@
 module.exports = {
   content: ["./frontEnd/**/*.{html,js}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        seagreen: '#2da57a',
+        'seagreen-dark': '#278e68' 
+      }
+    },
   },
   plugins: [],
 }
